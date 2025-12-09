c23e53eeeb775ff1acf8c6a4f0aad464ff65e9a6
Niccolò.Messina <niccolomessi25@gmail.com>
2025-12-09 12:27:58 +0100

    Adding components Analisi and Competenze

--- buildweek_3/src/components/Profile.jsx
+++ buildweek_3/src/components/Profile.jsx
@@ -1,11 +1,19 @@
 import { Container, Row, Col } from 'react-bootstrap'
+<<<<<<< Updated upstream
 import Navbar from './NavBar'
+=======
+import Analisi from './Analisi'
+import Competenze from './Competenze'
+>>>>>>> Stashed changes
 const Profile = function () {
   return (
     <Container>
       <Navbar/>
       <Row>
-        <Col xs={12} md={8}></Col>
+        <Col xs={12} md={8}>
+        <Analisi/>
+        <Competenze/>
+        </Col>
         <Col xs={12} md={4}></Col>
       </Row>
     </Container>
