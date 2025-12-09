97bba271e630f8d3f172f8eee153eb893f7d0e5e
Gentjana <hanigentjana@gmail.com>
2025-12-09 12:56:34 +0100

    adds_

--- buildweek_3/src/components/HeroSection.jsx
+++ buildweek_3/src/components/HeroSection.jsx
@@ -1,21 +1,37 @@
-import { Button, Container, Row, Col } from "react-bootstrap"
+import { Button, Container, Row, Col, Image } from "react-bootstrap"
 import { LuPencil } from "react-icons/lu"
 import { PiBuildingsDuotone } from "react-icons/pi"
 import { BiSolidBadgeCheck } from "react-icons/bi"
+import { FaPlus } from "react-icons/fa6"
 
 const HeroSection = function () {
     return (
 
-        <Container className="my-3 border rounded-3 border-tertiary">
+        <Container className="my-3 border rounded-3 border-secondary">
             <Row>
-                <Col xs={12} className="backGround">
-                    {/* <img src= alt="un orso" /> */}
+                <Col xs={12} className="backGround position-relative">
+                    {/* sfondo copertina in bg */}
+                    <div className="position-absolute top-100 start-0 translate-middle-y ms-3">
+                        <Image
+                            src="https://placebear.com/100/100"
+                            alt="img profilo"
+                            roundedCircle
+                            className="shadow"
+                        />
+                        <FaPlus
+                            className="position-absolute bottom-0 end-0 bg-white rounded-circle p-1 border"
+                            size={24}
+                        />
+                    </div>
                 </Col>
-                <Col xs={12} className="d-flex w-100 justify-content-end">
+                <Col xs={12} className=" my-3 d-flex w-100 justify-content-end">
                     {/* matita */}
                     <LuPencil className="fs-5" />
 
                 </Col>
+                {/* foto profilo */}
+
+
                 <Row className="align-items-center">
                     <Col xs={9} className="d-flex flex-wrap justify-content-between">
                         <h4>Nome Cognome</h4>
@@ -34,7 +50,7 @@ const HeroSection = function () {
                         <p>Roma, Lazio, Italia - <a>Infomazioni di contatto</a></p>
                     </Col>
                 </Row>
-                <Col xs={12}>
+                <Col xs={12} className="my-3">
                     <Row className="g-3">
                         <Col xs={3} lg={3}>
                             <Button variant="primary" className="rounded-pill fw-medium">
