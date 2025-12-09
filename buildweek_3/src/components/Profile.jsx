8c0ace41864d4486aff11553ecae59420817e3c7
Giada <tonni.giada.210802@gmail.com>
2025-12-09 13:55:59 +0100

    AGGIUNTA ATTIVITA

--- buildweek_3/src/components/Profile.jsx
+++ buildweek_3/src/components/Profile.jsx
@@ -1,43 +1,24 @@
 import { Container, Row, Col } from 'react-bootstrap'
-<<<<<<< Updated upstream
-import HeroSection from "./HeroSection"
-
+import HeroSection from './HeroSection'
 import Sidebar from './Sidebar'
-
+import Attivita from './Attivita'
 import Analisi from './Analisi'
 import Competenze from './Competenze'
 
 const Profile = function () {
   return (
     <Container>
-
-      <Row style={{ marginTop: "5em" }}>
+      <Row style={{ marginTop: '5em' }}>
         <Col xs={12} md={8}>
           <HeroSection />
           <Analisi />
           <Competenze />
+          <Attività />
         </Col>
         <Col xs={12} md={4}>
           <Sidebar />
-=======
-import Navbar from './NavBar'
-import Analisi from './Analisi'
-import Competenze from './Competenze'
-import Attivita from './Attivita'
-const Profile = function () {
-  return (
-    <Container>
-      <Navbar />
-      <Row>
-        <Col xs={12} md={8}>
-        
-          <Analisi />
-          <Competenze />
-          <Attivita/>
->>>>>>> Stashed changes
         </Col>
       </Row>
-
     </Container>
   )
 }
