d095d7d58eae39b125ed40500ec0efd772711418
Gentjana <hanigentjana@gmail.com>
2025-12-09 13:06:09 +0100

    updates

--- buildweek_3/src/App.jsx
+++ buildweek_3/src/App.jsx
@@ -1,12 +1,15 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Profile from './components/Profile'
 import { BrowserRouter } from 'react-router-dom'
-
+import Footer from "./components/Footer"
+import NavBar from "./components/NavBar"
 
 function App() {
   return (
     <BrowserRouter>
+      <NavBar />
       <Profile />
+      <Footer />
     </BrowserRouter>
   )
 }
--- buildweek_3/src/components/Profile.jsx
+++ buildweek_3/src/components/Profile.jsx
@@ -2,21 +2,25 @@ import { Container, Row, Col } from 'react-bootstrap'
 import HeroSection from "./HeroSection"
 
 import Sidebar from './Sidebar'
-import Navbar from './NavBar'
+
 import Analisi from './Analisi'
 import Competenze from './Competenze'
 
 const Profile = function () {
   return (
     <Container>
-      <Navbar />
-      <Row>
+
+      <Row style={{ marginTop: "5em" }}>
         <Col xs={12} md={8}>
+          <HeroSection />
           <Analisi />
           <Competenze />
         </Col>
-        <Col xs={12} md={4}></Col>
+        <Col xs={12} md={4}>
+          <Sidebar />
+        </Col>
       </Row>
+
     </Container>
   )
 }
