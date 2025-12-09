7a06e780cb0e2843373697496a660b97a475a554
Giada <tonni.giada.210802@gmail.com>
2025-12-09 12:49:22 +0100

    SIDEBAR E FOOTER CONCLUSI

--- buildweek_3/src/App.jsx
+++ buildweek_3/src/App.jsx
@@ -1,8 +1,13 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Profile from './components/Profile'
+import Footer from './components/Footer'
 
 function App() {
-  return <Profile />
+  return (
+    <>
+      <Profile />
+    </>
+  )
 }
 
 export default App
--- buildweek_3/src/components/Profile.jsx
+++ buildweek_3/src/components/Profile.jsx
@@ -1,4 +1,5 @@
 import { Container, Row, Col } from 'react-bootstrap'
+import Sidebar from './Sidebar'
 
 const Profile = function () {
   return (
