dc50d4927237d743e197a7fa51a01db184881071
Lore1993 <lorenzo93pellegrini@gmail.com>
2025-12-09 11:52:28 +0100

    NavBar

--- buildweek_3/src/components/Profile.jsx
+++ buildweek_3/src/components/Profile.jsx
@@ -1,8 +1,9 @@
 import { Container, Row, Col } from 'react-bootstrap'
-
+import Navbar from './NavBar'
 const Profile = function () {
   return (
     <Container>
+      <Navbar/>
       <Row>
         <Col xs={12} md={8}></Col>
         <Col xs={12} md={4}></Col>
